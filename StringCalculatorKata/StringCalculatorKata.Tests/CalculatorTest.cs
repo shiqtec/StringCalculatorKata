a59bd2cdfeb21a59173cb68cd28008fbe6c69c06
Ashiq <shiqtec@gmail.com>
2021-07-01 21:19:34 +0200

    Add empty string returns 0

--- StringCalculatorKata/StringCalculatorKata.Tests/CalculatorTest.cs
+++ StringCalculatorKata/StringCalculatorKata.Tests/CalculatorTest.cs
@@ -4,5 +4,18 @@ namespace StringCalculatorKata.Tests
 {
     public class CalculatorTest
     {
+
+        private readonly Calculator _calculator;
+
+        public CalculatorTest()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void EmptyStringReturns0()
+        {
+            Assert.Equal(0, _calculator.Add(""));
+        }
     }
 }
--- StringCalculatorKata/StringCalculatorKata/Calculator.cs
+++ StringCalculatorKata/StringCalculatorKata/Calculator.cs
@@ -2,5 +2,9 @@
 {
     public class Calculator
     {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
     }
 }
