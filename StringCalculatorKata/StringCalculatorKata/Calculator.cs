026288b91e0273d6a1cde05a3ee74f67c9ea7d56
Ashiq <shiqtec@gmail.com>
2021-07-05 21:14:06 +0200

    Refactor support for custom delimiter

--- StringCalculatorKata/StringCalculatorKata/Calculator.cs
+++ StringCalculatorKata/StringCalculatorKata/Calculator.cs
@@ -7,27 +7,28 @@ namespace StringCalculatorKata
     {
         public int Add(string numbers)
         {
-            if (numbers is not { Length: > 0 })
+            if (numbers.Length == 0)
             {
                 return 0;
             }
 
-            var delimiters = Array.Empty<char>();
+            var delimiterAndNumbersString = GetDelimiterAndNumbersString(numbers);
+            numbers = delimiterAndNumbersString.Item2;
 
-            if(numbers.StartsWith("//"))
-            {
-                delimiters = numbers.Substring(2, 1).ToCharArray();
-
-                var numbersStartPosition = numbers.IndexOf("\n") + 1;
-                numbers = numbers.Substring(numbersStartPosition);
-            } else
-            {
-                delimiters = new char[] { ',', '\n' };
-            }
-
-            return numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+            return numbers.Split(delimiterAndNumbersString.Item1)
                           .Select(number => Convert.ToInt32(number))
                           .Sum();
+        }
+
+        public (char[], string) GetDelimiterAndNumbersString(string numbers)
+        {
+            if(numbers.StartsWith("//"))
+            {
+                var numbersStartPosition = numbers.IndexOf("\n") + 1;
+                return (numbers.Substring(2, 1).ToCharArray(), numbers.Substring(numbersStartPosition));
+            }
+
+            return (new char[] { ',', '\n' }, numbers);
         }
     }
 }
